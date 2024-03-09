ef57db8c2d87f4c545277050259be44bbc36e7ea
svivek19 <svivek030503@gmail.com>
2024-03-09 18:44:15 +0530

    navbar finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./component/Navbar";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-900 p-5 text-xl text-white text-center font-semibold">
+      <div>TheGoodGame Theory - Task</div>
+    </nav>
+  );
+};
+
+export default Navbar;
