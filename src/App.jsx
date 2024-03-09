9560912805196bca1112d1b7adfd1310ffa257af
svivek19 <svivek030503@gmail.com>
2024-03-09 20:27:56 +0530

    data fetch from api and display the details finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./component/Navbar";
 import SearchInput from "./component/SearchInput";
 import Card from "./component/Card";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.punkapi.com/v2/beers");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Navbar />
       <SearchInput />
-      <Card />
+      <Card data={data} />
     </>
   );
 };
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,59 +1,31 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ data }) => {
   return (
     <div className="w-5/6 mx-auto grid grid-cols-3 gap-4">
-      <div className="p-4">
-        <div className="w-5/6 rounded overflow-hidden shadow-lg">
-          <img
-            className="w-full h-64 object-cover"
-            src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZXxlbnwwfHwwfHx8MA%3D%3D"
-            alt="Sunset"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p className="text-gray-700 text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
+      {data.map((beer, index) => (
+        <div className="p-4" key={index}>
+          <div className="w-5/6 rounded overflow-hidden shadow-lg">
+            <img
+              className="w-full h-64 object-cover"
+              src={beer.image_url}
+              alt="Sunset"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">
+                {beer.name.length > 20
+                  ? beer.name.slice(0, 20) + "..."
+                  : beer.name}
+              </div>
+              <p className="text-gray-700 text-justify">
+                {beer.description.length > 30
+                  ? beer.description.slice(0, 15) + "..."
+                  : beer.description}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="p-4">
-        <div className="w-5/6 rounded overflow-hidden shadow-lg">
-          <img
-            className="w-full h-64 object-cover"
-            src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZXxlbnwwfHwwfHx8MA%3D%3D"
-            alt="Sunset"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p className="text-gray-700 text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="p-4">
-        <div className="w-5/6 rounded overflow-hidden shadow-lg">
-          <img
-            className="w-full h-64 object-cover"
-            src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZXxlbnwwfHwwfHx8MA%3D%3D"
-            alt="Sunset"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p className="text-gray-700 text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
