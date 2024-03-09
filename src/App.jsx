815c5fb0e75fe8eab50781ae81f84d2c037a39f5
svivek19 <svivek030503@gmail.com>
2024-03-09 18:50:15 +0530

    search input style finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./component/Navbar";
+import SearchInput from "./component/SearchInput";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <SearchInput />
     </>
   );
 };
---
+++ src/component/SearchInput.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SearchInput = () => {
+  return (
+    <div className="w-5/6 mx-auto my-10 text-center">
+      <form className="space-x-3">
+        <input
+          type="search"
+          placeholder="Search Items..."
+          className="bg-gray-200 font-semibold outline-none p-2 w-2/6 rounded"
+        />
+        <button className="bg-slate-700 px-5 py-2 rounded text-white font-semibold">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchInput;
